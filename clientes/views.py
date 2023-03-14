bf63e29f5a92fd78bd1954f2356bd77c95f40e8d
Fabricio Lopes <fabriciolopesm@gmail.com>
2023-03-13 23:48:27 -0300

    criado o atualiza cliente

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Cliente, Carro
 import re
+from django.core import serializers
+import json
 
 def clientes(request):
     if request.method == "GET":
@@ -40,3 +42,9 @@ def clientes(request):
             car.save()
 
         return HttpResponse('teste')
+
+def att_cliente(request):
+    id_cliente = request.POST.get('id_cliente')
+    cliente = Cliente.objects.filter(id=id_cliente)
+    cliente_json = json.loads(serializers.serialize('json', cliente))[0]['fields']
+    return JsonResponse(cliente_json)
--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -1,7 +1,6 @@
 function add_carro(){
     
     container = document.getElementById('form-carro')
-
     html = "<br>  <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro'> </div> <div class='col-md'> <input type='text' placeholder='Placa' class='form-control' name='placa'> </div> <div class='col-md'> <input type='number' placeholder='ano' class='form-control' name='ano'> </div> </div>"
 
     container.innerHTML += html
@@ -20,5 +19,43 @@ function exibir_form(tipo){
         att_cliente.style.display = "block"
         add_cliente.style.display = "none"
     }
+
+}
+
+
+function dados_cliente(){
+    cliente = document.getElementById('cliente-select')
+    csrf_token = document.querySelector('[name=csrfmiddlewaretoken]').value
+    id_cliente = cliente.value
+
+    data = new FormData()
+    data.append('id_cliente', id_cliente)
+
+    fetch("/clientes/atualiza_cliente/",{
+        method: "POST",
+        headers: {
+            'X-CSRFToken': csrf_token,
+        },
+        body: data
+
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+        
+        document.getElementById('form-att-cliente').style.display = 'block'
+        
+        nome = document.getElementById('nome')
+        nome.value = data['nome']
+        
+        sobrenome = document.getElementById('sobrenome')
+        sobrenome.value = data['sobrenome']
+        
+        email = document.getElementById('email')
+        email.value = data['email']
+        
+        cpf = document.getElementById('cpf')
+        cpf.value = data['cpf']
+
+    })
 }
         
